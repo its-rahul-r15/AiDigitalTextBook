6b15d27b3f836854cabb75ae93e78241d76df200
Ritesh-tech <aritesh400@gmail.com>
2026-02-24 20:27:00 +0530

    some new changes

---
+++ server/src/utils/cloudinary.utils.js
@@ -0,0 +1,9 @@
+Import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+Cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME
+    api_key:process.env.CLOUDINARY_API_KEY
+    api_secret:process.env.CLOUDINARY_API_SECRET
+
+})
